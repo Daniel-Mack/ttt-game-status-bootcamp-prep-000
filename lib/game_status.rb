65b7a6c416b8436b77ccb3b6e9ed14b336679d41
Daniel Mack <daniel.mack@powerhrg.com>
2018-04-21 17:42:19 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,7 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
+<<<<<<< HEAD
 [0, 1 ,2],
 [3, 4, 5],
 [6, 7, 8],
@@ -13,10 +14,21 @@ WIN_COMBINATIONS = [
 [2, 5, 8],
 [0, 4, 8],
 [6, 4, 2]
+=======
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+>>>>>>> 1d76900ae0aa46ee6f1dd989d0836cba29f411ad
 ]
 
 
 def won?(board)
+<<<<<<< HEAD
   WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
@@ -61,3 +73,7 @@ def over?(board)
       board[winning_combination.first]
     end
   end
+=======
+
+end
+>>>>>>> 1d76900ae0aa46ee6f1dd989d0836cba29f411ad
